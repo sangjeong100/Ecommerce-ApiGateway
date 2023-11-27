671f1d46f8e6df858190bc2cf7ab408c38cf9cc1
sjyu <sjyu@i-leaders.co.kr>
2023-11-27 20:39:41 +0900

    feat : spring config 설정 추가

--- src/main/java/com/ecommerce/apigatewayservice/EcommerceApiGatewayServiceApplication.java
+++ src/main/java/com/ecommerce/apigatewayservice/EcommerceApiGatewayServiceApplication.java
@@ -1,7 +1,12 @@
 package com.ecommerce.apigatewayservice;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTrace;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EcommerceApiGatewayServiceApplication {
@@ -10,4 +15,22 @@ public class EcommerceApiGatewayServiceApplication {
 		SpringApplication.run(EcommerceApiGatewayServiceApplication.class, args);
 	}
 
+	// for httptrace
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new HttpTraceRepository() {
+			
+			@Override
+			public List<HttpTrace> findAll() {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			
+			@Override
+			public void add(HttpTrace trace) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+	}
 }
